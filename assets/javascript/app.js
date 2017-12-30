ebcc732d8d97d961303b758f83e3e47df299906d
Anne <coloradoz99@gmail.com>
2017-12-29 20:06:21 -0700

    updates to array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // make an array of giphs 
-var topic = ["aardvark", "bunny", "cat", "dog", "emu", "fox", "giraffe"," hippo", "impala" ," kangaroo", "lion", "manatee" ,"narwhal" ];
+var topic = ["shocked dog", "baby", "puppy", "Ryan Gosling ", "Love gnome", "waving", "Llama"," Reaction", "Good Morning ","Happy Dance", "Nope", "Chris Hemsworth","narwhal","Robert Redford" ];
 // console.log("this is giphys:  "+ topic);
 //function to display giphy buttons 
 function renderButtons(){
@@ -8,23 +8,12 @@ function renderButtons(){
   //loop array to add movies 
   for (var i = 0; i < topic.length; i++) {
     var gifButton = '<button class="gif" data-name="' + topic[i] + '">' + topic[i] +'</button>';
-  
-// //    generate buttons 
-//     var a =$("<button>");
-//     //add class
-//     a.addClass("gif");
-//     //use data-name attribute for giphy search 
-//     a.attr("data-name", topic[i]);
-//     //add button text from input box
-//     a.text(topic[i]);
-//   //  add the button to the html
+  // //    generate buttons 
    $("#giphy-view").append(gifButton);
   }
 };
 // call the render buttons function to process the array 
   renderButtons();
-
-  //***** ******** 
 //function for adding user created button 
 $("#add-giphy").on("click",function(event){
   //stop form from submitting itself
@@ -34,25 +23,19 @@ $("#add-giphy").on("click",function(event){
   // $(giphy).addClass("gif");
     //add var giphy to the topic array
   topic.push(giphy);
-
   $("#giphy-input").val("");
   //call the renderButtons function to display the original array of topics
   renderButtons();
 });
-
-//********* */
-//create a function to query ajax and present gif with rating on the rendered Buttons 
  // Adding click event listen listener to all buttons
  $("#giphy-view").on("click", ".gif", function() {
    console.log("this was clicked");
   $("#giphy-div").empty();
     // Grabbing and storing the data property value from the button
   var searchTerm = $(this).attr("data-name");
-
   // Constructing a queryURL using the searchTerm name
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
   searchTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
-
   // Performing an AJAX request with the queryURL
   $.ajax({
       url: queryURL,
@@ -60,10 +43,7 @@ $("#add-giphy").on("click",function(event){
     })
     // After data comes back from the request
         .done(function(response) {
-          // console.log(queryURL);
-          // console.log(response);
-          // storing the data from the AJAX request in the results variable
-          // var results = response.data;
+
           // Looping through each result item
           for (var i = 0; i < response.data.length; i++) {
             // Creating and storing a div tag
@@ -84,19 +64,15 @@ $("#add-giphy").on("click",function(event){
                // Appending the paragraph and image tag to the gifDiv
                gifDiv.append(p);
              gifDiv.append(gifImage);
-
             // Prependng the gifDiv to the HTML page in the "#giphys" div
             $("#giphy-div").prepend(gifDiv);
-
           }
                  // call .gif class to add animation */
                  $(".gif").on("click", function() {
-                   
-                // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+                 
+                // The attr jQuery method to get or set the value of  attribute on HTML 
                 var state = $(this).attr('data-state');
-                // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-                // Then, set the image's data-state to animate
-                // Else set src to the data-still value
+                // If the clicked image's state is still, update its src attribute to what its data-animate value is.Then, set the image's data-state to animate,Else set src to the data-still value
                  if (state === "still") {
                  $(this).attr("data-state", "animate");
                  $(this).attr("src", $(this).attr("data-animate"));
